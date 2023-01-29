e629348c62b52116d012770d5e49fe86a4725c54
Tomas Podesta <tomasagustinpodesta@gmail.com>
2023-01-29 12:20:24 -0300

    Prefabs, Structures, Ships, music, effects
    I setted up all the infrastructure that I need in order to complete the proyect.

---
+++ RailsShooter/Assets/MusicPlayer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start(){
+        Invoke("LoadFirstScene", 2f);
+    }
+
+    // Update is called once per frame
+    void LoadFirstScene(){
+          SceneManager.LoadScene(1);
+    }
+}
